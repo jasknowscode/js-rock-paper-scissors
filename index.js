4e0e9efa0d391e94e7ebdb38b2f36620c616c89a
Jasmine <jasmineparks991@gmail.com>
2026-04-04 15:36:35 -0500

    Fix console log for tie.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-    // Generate three random computer choices "Rock, Paper, Scissors" //
+        // Generate three random computer choices "Rock, Paper, Scissors" //
+
         function getComputerChoice() {
             const x =  Math.random() * 3;
 
@@ -13,45 +14,52 @@
             }
         }; 
 
-            console.log(getComputerChoice()); 
-        // Prompt user to enter three choices making the answer case sensitive//
+        //   console.log(getComputerChoice()); //
+        // Prompt user to enter three choices making the answer case sensitive //
+
         function getHumanChoice() {
+
             const answer = (prompt("Please enter your choice of Rock, Paper, or Scissors"));
-                return answer.toLowerCase();
+                console.log(answer);
         };
 
         // console.log(getHumanChoice()); //
-        
-        // Take human and computer choices and compare. Winning combinations return "winner" and increment score by one. //
+
         function playGame(playRound) {
 
-            humanScore = 0;
-            computerScore = 0;
+        // Starting score for human and computer //
+
+        let humanScore = 0;
+        let computerScore = 0;
+
+        // Take human and computer choices and compare. Winning combinations return "winner" and increment score by one. //
+        // Log the round winner //
 
             function playRound(humanChoice, computerChoice) {
 
                 if  (humanChoice === computerChoice) { 
-                    return "It's a tie";
+                    console.log("It's a tie");
                 }
                 else if ((humanChoice === "rock" && computerChoice === "scissors") ||
                          (humanChoice === "paper" && computerChoice === "rock") || 
                          (humanChoice === "scissors" && computerChoice === "paper")) {
+                    console.log("You're the winner of this round," + " " + humanChoice + " " + "beats" + " " + computerChoice)
                     humanScore++;
-                    console.log("You win!");
                 }
                 else {
+                    console.log("The computer wins this round!");
                     computerScore++;
-                    console.log("Computer wins!");
                 }
             };
+            console.log(playRound())
 
             const humanSelection = getHumanChoice();
-            const computerSelection = getComputerChoice();
+            const computerSelection = getComputerChoice();  
 
-            console.log(playRound(humanSelection, computerSelection));
-
-            // Play five rounds calling choice function result for each round //
+        // Play five rounds calling choice function result for each round //
         }
 
-        console.log(playGame());
+         console.log(playGame());
+
+       
         
