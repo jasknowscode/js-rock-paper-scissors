58ecb8adf38880927782cb1aa9a49e18e5b1bd10
Jasmine <jasmineparks991@gmail.com>
2026-05-06 21:24:44 -0500

    Fix score display to show round and game winners.

--- index.js
+++ index.js
@@ -1,67 +1,71 @@
-        
         // Create a function that takes a number between 0 and 3 (1, 2, 3)
         // And rounds to the nearest whole number to return a string
         // Return a string of 3 outcomes "Rock, Paper, Scissors"
 
-        let computerChoice =  Math.floor().Math.random(1, 2, 3) * 3;
-        let humanChoice = prompt("Please enter your choice of Rock, Paper, or Scissors")
-            answer.toUpperCase();
+let humanScore = 0;
+let computerScore = 0;
 
-        let humanScore = 0;
-        let computerScore = 0;
-
-        function getComputerChoice() {
-
-            if (computerChoice <= 1) {
-                return ("ROCK");
-            }
-            if (computerChoice >= 2) {
-                return ("PAPER");
-            }
-            else {
-                return ("SCISSORS");
-            }
-
-        };
-        
-        function playGame(playRound) {
-
-            for (let playRound = 0; ; playRound++) {
-
-                if (humanScore > computerScore) {
-                playRound = humanScore++;
-                }
-                if (computerScore > humanScore) {
-                playRound = computerScore++;
-                }
-            };
-
-            function playRound(humanChoice, computerChoice) {
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper');
+const scissorsBtn = document.getElementById('scissors');
 
 
-                if  (humanChoice === computerChoice) {
-                    playRound.textContent = "It's a tie";
-                } else if ((humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
-                           (humanChoice === "PAPER" && computerChoice === "ROCK") || 
-                           (humanChoice === "SCISSORS" && computerChoice === "PAPER")) {
-                    playRound.textContent = ("You're the winner of this round, ") + humanChoice  + (" beats ") + computerChoice;
-                    humanScore++;
-                } else {
-                    playRound.textContent = "The computer wins this round!";
-                    computerScore++;
-                }
-            };
-        }
-        
-        const rockBtn = document.getElementById('#rock');
-        const paperBtn = document.getElementById('#paper');
-        const scissorsBtn = document.getElementById('#scissors');
+const results = document.createElement('div');
+results.textContent = "SCORE";
+results.style.backgroundColor = "pink";
+const scoreDisplay = document.createElement('div');
+scoreDisplay.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
 
-        rockBtn.addEventListener('click', () => playRound('ROCK'));
-        paperBtn.addEventListener('click', () => playRound('PAPER'));
-        scissorsBtn.addEventListener('click', () => playRound('SCISSORS'));
 
-        const results = document.getElementById('#results');
+
+function updateScore() {
+    scoreDisplay.textContent = `You: ${humanScore} | Computer: ${computerScore}`;
+
+    if (humanScore === 5) {
+        scoreDisplay.textContent = "That's game, you win!";
+    } else if (computerScore === 5) {
+        scoreDisplay.textContent = "That's game, computer wins!";
+    } 
+};
+
+document.body.appendChild(results);
+document.body.appendChild(scoreDisplay);
+
+function getComputerChoice() {
+    let computerChoice =  Math.floor(Math.random() * 3);
+
+    if (computerChoice === 1) {
+        return ("ROCK");
+    } if (computerChoice === 2) {
+        return ("PAPER");
+    } else {
+        return ("SCISSORS");
+    }
+};
+
+function playRound(humanChoice) {
+    const computerChoice = getComputerChoice();
+
+    if  (humanChoice === computerChoice) {
+        results.textContent = "It's a tie";
+    } else if (
+        (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
+        (humanChoice === "PAPER" && computerChoice === "ROCK") || 
+        (humanChoice === "SCISSORS" && computerChoice === "PAPER")) {
+        results.textContent = ("You're the winner of this round, ") + humanChoice  + (" beats ") + computerChoice;
+        humanScore++;
+        updateScore();
+    } else {
+        results.textContent = ("The computer wins this round!");
+        computerScore++;
+        updateScore();
+    }
+};
+
+
+rockBtn.addEventListener('click', () => playRound("ROCK"));
+paperBtn.addEventListener('click', () => playRound("PAPER"));
+scissorsBtn.addEventListener('click', () => playRound("SCISSORS"));
 
         // Prompt user to enter three choices making the answer case sensitive
         // Return value to of human choice
